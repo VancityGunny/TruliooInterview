f5652d67f55de9b2ee77b2cd863aae25cc3d686a
Gunny <vancitygunny@gmail.com>
2023-03-21 00:22:42 -0700

    remove unused code

--- test/index.test.js
+++ test/index.test.js
@@ -116,26 +116,3 @@ describe('Registration endpoint', () => {
         expect(response.body.errors[0].msg).to.equal('Lastname must be between 3 and 20 characters');
       });
     });
-
-
-// describe('POST /api/register', () => {    
-//     it('successfully created new user', async () =>{        
-//         const res = await request(app).post('/api/register').send({
-//             "username":"gunny",
-//             "firstname": "Gunny",
-//             "lastname": "Baby",
-//             "email": "gunny@hey.com",
-//             "password": "12345678"
-//         });
-//         expect(res.status).to.equal(200);
-//     });
-
-//     it('successfully login', async () =>{        
-//         const res = await request(app).post('/api/login').send({
-//             "email": "gunny@hey.com",
-//             "password": "12345678"
-//         });
-//         expect(res.status).to.equal(200);
-//     });
-// })
-
